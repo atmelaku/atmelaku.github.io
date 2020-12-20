8d2f874ec12806386017dfe8ad5f1196335e590c
Melaku <alebachewtegegne2017@yahoo.com>
2020-12-19 22:14:42 -0500

    add images in to asset folder update home page update about page update projects page update resume page update favorite page update contact add a new skill page update css

--- main.js
+++ main.js
@@ -1,41 +1,4 @@
-// // get the element first
-// const imgme = document.getElementById("mee");
-// const mainEl = document.querySelector('main')
-// const parEl = document.getElementById("Copyright");
-// parEl.style.color = "blue";
-//
-// // hover effect on the picture
-//
-// function show_image(src) {
-//
-//   const img = document.createElement("img");
-//   imgme.style.border = "solid red"
-//   img.src = "assets/github-mark.png";
-//
-//   imgme.appendChild(img);
-// }
-//
-// imgme.addEventListener("mousemove", show_image )
-// // random background color
-//
-// mainEl.style.backgroundColor = "red";
-//
-// const newp = document.createElement("button");
-// const text = document.createTextNode("changeBgColor");
-//
-// newp.appendChild(text)
-// mainEl.appendChild(newp);
-// //
-// // function random(number) {
-//
-//   return Math.floor(Math.random() * (number+1));
-//    }
-//
-// newp.onclick = function() {
-//
-//   const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-//   document.body.style.backgroundColor = rndCol;
-//    }
+
 const pEl = document.getElementById("adress");
    pEl.style.visibility = "hidden";
    const imgEl = document.getElementById("imag");
